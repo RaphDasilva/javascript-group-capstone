baa615897a8d64b4bfb3c9baa4026dc66a800616
Raphael <okolonnaemeka2017@gmail.com>
2023-02-07 07:34:08 -0500

    Add function for displaying movies.

---
+++ src/modules/show-movies.js
@@ -0,0 +1,21 @@
+import movieContainer from './project-const';
+import createMovies from './display-movies';
+
+const showMovies = async () => {
+  movieContainer.innerHTML = '';
+  const movieArr = await createMovies();
+  movieArr.forEach((element) => {
+    movieContainer.innerHTML += `
+     <li class = "movie-${element.id}">
+                <img src="${element.img}" alt="${element.name}" width="200px">
+                <div>
+                    <p>${element.name}</p>
+                    <span><i data-id="${element.id}" class="fa-regular fa-heart like-icon"></i> 5 likes</span>
+                </div>
+                <button id = ${element.id}>Comments</button>
+            </li>
+     `;
+  });
+};
+
+export default showMovies;
